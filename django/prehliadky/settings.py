eec5eed5560642ebf1bbdca0bb4aa0a5b6f7c9fa
MatusB <matus.bystricky@gmail.com>
2022-11-06 17:10:39 +0100

    kofiguracia pripojenia na mysql databazu issue #25

--- django/prehliadky/settings.py
+++ django/prehliadky/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'prehliadky.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',          # _psycopg2
+        'NAME': 'prehliadky',
+        'USER': 'prehliadky',
+        'PASSWORD': 'Gv3yjnctg:',
+        'HOST': 'mysql80.r2.websupport.sk',
+        'PORT': '3314',
     }
 }
 
