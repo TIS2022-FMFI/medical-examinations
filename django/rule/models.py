0a76c763ee495584a23764f598122cb0d7d26a15
= <kocalka.matus.ma@gmail.com>
2022-12-09 21:24:34 +0100

    kompletna prerabka aplikacii Employee a Rule #56

--- django/rule/models.py
+++ django/rule/models.py
@@ -1,25 +1,37 @@
 from django.db import models
 
-
-class RuleType(models.Model):
-    name = models.CharField(max_length=150)                                 # varchar(150) NOT NULL
-
-    def __str__(self):
-        return self.name
-
-
-class Locality(models.Model):
-    name = models.CharField(max_length=150)                                 # varchar(150) NOT NULL
+class City(models.Model):
+    name = models.CharField(max_length=50)                                 # varchar(150) NOT NULL
 
     def __str__(self):
         return self.name
 
 
-class Rule(models.Model):
-    ruleTypeId = models.ForeignKey(RuleType, on_delete=models.DO_NOTHING)   # FOREIGNKEY NOT NULL
-    localityId = models.ForeignKey(Locality, on_delete=models.DO_NOTHING)   # FOREIGNKEY NOT NULL
-    name = models.CharField(max_length=150)                                 # varchar(150) NOT NULL
+class Department(models.Model):
+    name = models.CharField(max_length=50)                                 # varchar(150) NOT NULL
+    cityId = models.ForeignKey(City, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return f'{self.name} - {self.ruleTypeId} - {self.localityId}'
+        return f"{self.cityId} {self.name}"
+
+class Rule(models.Model): # abstration
+    ...
+
+class PositionRule(models.Model):
+    name = models.CharField(max_length=100)
+    departmentId = models.ForeignKey(Department, on_delete=models.DO_NOTHING)
+    ruleId = models.ForeignKey(Rule, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"{self.departmentId} {self.name}"
+
+class ShiftRule(models.Model):
+    name = models.CharField(max_length=150)
+    ruleId = models.ForeignKey(Rule, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class HiddenRule(models.Model):
+    ruleId = models.ForeignKey(Rule, on_delete=models.DO_NOTHING)
 
