e7a9175e477f9957db0999771e1983794c002eef
FrankerSK FrankerSK <frank16@uniba.sk>
2022-12-13 11:53:08 +0100

    Pridany button na add prehliadky, opraveny potrebny login na examinationtype, Issue #60

--- django/examinationType/views.py
+++ django/examinationType/views.py
@@ -5,34 +5,36 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from django.urls import reverse_lazy
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import ExaminationType
 
 
-class ExaminationTypeList(ListView):
+class ExaminationTypeList(LoginRequiredMixin, ListView):
     model = ExaminationType
     context_object_name = 'examinatonTypes'
 
 
-class ExaminationTypeDetail(DetailView):
+class ExaminationTypeDetail(LoginRequiredMixin, DetailView):
     model = ExaminationType
     context_object_name = 'examinatonType'
     # template = 'templates\employee\employee_detail'
 
 
-class ExaminationTypeCreate(CreateView):
+class ExaminationTypeCreate(LoginRequiredMixin, CreateView):
     model = ExaminationType
     fields = '__all__'
     success_url = reverse_lazy('examinatontypes')
 
 
-class ExaminationTypeUpdate(UpdateView):
+class ExaminationTypeUpdate(LoginRequiredMixin, UpdateView):
     model = ExaminationType
     fields = '__all__'
     success_url = reverse_lazy('examinatontypes')
     context_object_name = 'examinatonType'
 
 
-class ExaminationTypeDelete(DeleteView):
+class ExaminationTypeDelete(LoginRequiredMixin, DeleteView):
     model = ExaminationType
     context_object_name = 'examinatonType'
     success_url = reverse_lazy('examinationtypes')
