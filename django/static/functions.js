cc44dceb1bcea2aee48d7fb54921cb9f57f5c0ad
Matus <matus.bystricky@gmail.com>
2023-02-09 18:38:25 +0100

    opravene chybicky

--- django/static/functions.js
+++ django/static/functions.js
@@ -1,47 +1,46 @@
 
 function search() {
-  var input1, input2, input3, input4, input5, input6, filter1, filter2, filter3, filter4, filter5, filter6, table, tr, td1, td2, td3, td4, td5, td6, i;
-  input1 = document.getElementById("name");
-  input2 = document.getElementById("id");
-  input3 = document.getElementById("posit");
-  input4 = document.getElementById("depart");
-  input5 = document.getElementById("city");
-  input6 = document.getElementById("shift");
-  filter1 = input1.value.toUpperCase();
-  filter2 = input2.value.toUpperCase();
-  filter3 = input3.value.toUpperCase();
-  filter4 = input4.value.toUpperCase();
-  filter5 = input5.value.toUpperCase();
-  filter6 = input6.value.toUpperCase();
-  table = document.getElementById("myList");
-  tr = table.getElementsByTagName("tr");
+    var input1, input2, input3, input4, input5, input6, filter1, filter2, filter3, filter4, filter5, filter6, table, tr, td1, td2, td3, td4, td5, td6, i;
+    input1 = document.getElementById("name");
+    input2 = document.getElementById("id");
+    input3 = document.getElementById("posit");
+    input4 = document.getElementById("depart");
+    input5 = document.getElementById("city");
+    input6 = document.getElementById("shift");
+    filter1 = input1.value.toUpperCase();
+    filter2 = input2.value.toUpperCase();
+    filter3 = input3.value.toUpperCase();
+    filter4 = input4.value.toUpperCase();
+    filter5 = input5.value.toUpperCase();
+    filter6 = input6.value.toUpperCase();
+    table = document.getElementById("myList");
+    tr = table.getElementsByTagName("tr");
 
-  for (i = 0; i < tr.length; i++) {
-    for (var n = 0; n < 6; n++){
-      td = tr[i].getElementsByTagName("td");
-      if(td){
-        tr[i].style.display = "table-row ";
-      }
+    for (i = 0; i < tr.length; i++) {
+        for (var n = 0; n < 6; n++){
+            td = tr[i].getElementsByTagName("td");
+            if(td){
+                tr[i].style.display = "table-row ";
+            }
+        }
     }
-  }
-
- // Loop through all table rows, and hide those who don't match the search query
- console.log("TEST");
- console.log(input1, input2, input3, input4, input5, input6);
- console.log("TEST");
- for (i = 0; i < tr.length; i++) {
-  if(input1.value === ""){
-    break;
-  }
-  td1 = tr[i].getElementsByTagName("td")[0];
-  if (td1) {
-    if (td1.innerHTML.toUpperCase().indexOf(filter1) > -1) {
-      continue;
-    } else {
-        tr[i].style.display = "none";
-      }
-  }
-}
+    // Loop through all table rows, and hide those who don't match the search query
+    console.log("TEST");
+    console.log(input1, input2, input3, input4, input5, input6);
+    console.log("TEST");
+    for (i = 0; i < tr.length; i++) {
+        if(input1.value === ""){
+            break;
+        }
+        td1 = tr[i].getElementsByTagName("td")[0];
+        if (td1) {
+            if (td1.innerHTML.toUpperCase().indexOf(filter1) > -1) {
+                continue;
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
 for (i = 0; i < tr.length; i++) {
   if(input2.value === ""){
     break;
